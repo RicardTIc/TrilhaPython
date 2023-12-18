98b406ac4254dd1e8e347ae1425bd59962586e0d
Ricardo <cat-richard@hotmail.com>
2023-12-17 23:53:18 -0300

    b

--- DataFruta/P001/__init__.py
+++ DataFruta/P001/__init__.py
@@ -58,10 +58,11 @@ class Data:
     def __lt__(self, outraData):
         return (self.__ano, self.__mes, self.__dia) < (outraData.__ano, outraData.__mes, outraData.__dia)
     
+     # Comparação para maior que entre duas datas
     def __gt__(self, outraData):
         return (self.__ano, self.__mes, self.__dia) > (outraData.__ano, outraData.__mes, outraData.__dia)
 
-
+# Definindo uma classe abstrata para análise de dados
 class AnaliseDados(ABC): 
     @abstractmethod
     def entrada_de_dados(self):
@@ -87,11 +88,12 @@ class AnaliseDados(ABC):
     def __iter__(self):
         pass
 
-
+# Classe para lidar com listas de nomes, herdando de AnaliseDados
 class ListaNomes(AnaliseDados):
     def __init__(self):
-        self.__lista = []        
-
+        self.__lista = [] 
+               
+    # Método para inserir nomes na lista
     def entrada_de_dados(self):
         try:
             quantidade = int(input("Quantos nomes deseja inserir? "))
